a2c899fb8671b27e56e1798687dc904719d4f40c
Fe2Cu <fectung@gmail.com>
2017-05-12 22:35:43 +0800

    The First Complete Changes

--- app/src/main/java/com/fec/ex/sensorz/MainActivity.java
+++ app/src/main/java/com/fec/ex/sensorz/MainActivity.java
@@ -1,21 +1,54 @@
 package com.fec.ex.sensorz;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+public class MainActivity extends AppCompatActivity {
     /**
      * 声明所需传感器
      */
-    private final SensorManager mSensorManager;
-    private final Sensor mAccelerometer;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mAccelerometer != null) {
+            mSensorManager.registerListener(mAccelerometerListener, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mAccelerometer != null) {
+            mSensorManager.unregisterListener(mAccelerometerListener);
+        }
+    }
+
+    private SensorEventListener mAccelerometerListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            Log.d("onSensorChanged: ", event.toString());
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            Log.d("onAccuracyChanged: ", sensor.toString() + " - " + accuracy);
+        }
+    };
 }
