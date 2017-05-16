121fed4aee376b897741da3fb5798226a12e5722
Fe2Cu <fectung@gmail.com>
2017-05-16 19:42:39 +0800

    Use RecyclerView and CardView design the main view

--- app/src/main/java/com/fec/ex/sensorz/SensorActivity.java
+++ app/src/main/java/com/fec/ex/sensorz/SensorActivity.java
@@ -1,13 +1,31 @@
 package com.fec.ex.sensorz;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SensorActivity extends AppCompatActivity {
+
+    private ArrayList<SensorItem> mSensors;
+
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    private List<Sensor> mSensorList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rvSensors);
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        mSensorList = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+
     }
 }
--- app/src/main/java/com/fec/ex/sensorz/SensorRvAdapter.java
+++ app/src/main/java/com/fec/ex/sensorz/SensorRvAdapter.java
@@ -1,8 +1,70 @@
 package com.fec.ex.sensorz;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
 /**
  * Created by USFec on 2017.05.16.
  */
 
-public class SensorRvAdapter {
+public class SensorRvAdapter extends RecyclerView.Adapter<SensorRvAdapter.ViewHolder> {
+
+    private List<SensorItem> mSensors;
+    private Context mContext;
+
+    public SensorRvAdapter(Context mContext, List<SensorItem> sensors) {
+        this.mContext = mContext;
+        this.mSensors = sensors;
+    }
+
+    public Context getContext() {
+        return mContext;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View sensorView = inflater.inflate(R.layout.item_sensor, parent, false);
+        ViewHolder viewHolder = new ViewHolder(sensorView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+        SensorItem sensorItem = mSensors.get(position);
+
+        TextView tvSensorName = holder.mTVSensorName;
+        TextView tvSensorValue = holder.mTVSensorValue;
+
+        tvSensorName.setText("Name");
+        tvSensorName.setText("Value");
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mSensors.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView mTVSensorName;
+        private TextView mTVSensorValue;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            mTVSensorName = (TextView) itemView.findViewById(R.id.tvSensorItemName);
+            mTVSensorValue = (TextView) itemView.findViewById(R.id.tvSensorItemValues);
+        }
+    }
 }
