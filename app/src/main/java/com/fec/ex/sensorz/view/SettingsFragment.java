f563551237107de2d2cfcb0416e22aab394ac75d
Fe2Cu <fectung@gmail.com>
2017-06-09 10:44:12 +0800

    New Settings

--- app/src/main/java/com/fec/ex/sensorz/view/SettingsFragment.java
+++ app/src/main/java/com/fec/ex/sensorz/view/SettingsFragment.java
@@ -1,3 +1,59 @@
 package com.fec.ex.sensorz.view;
 
-public class SettingsActivity
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.preference.Preference;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fec.ex.sensorz.R;
+import com.takisoft.fix.support.v7.preference.PreferenceFragmentCompat;
+
+public class SettingsFragment extends PreferenceFragmentCompat{
+
+    private Preference switchPreference;
+
+    public SettingsFragment() {}
+
+    @Override
+    public void onCreatePreferencesFix(@Nullable final Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.pref_settings, rootKey);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        switchPreference = getPreferenceManager().findPreference("pref_show_more");
+        switchPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                return true;
+            }
+        });
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        handleBackEvent();
+    }
+
+    private void handleBackEvent() {
+        getView().setFocusableInTouchMode(true);
+        getView().requestFocus();
+        getView().setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_UP && keyCode == KeyEvent.KEYCODE_BACK){
+                    getActivity().getSupportFragmentManager().popBackStack("SensorList", FragmentManager.POP_BACK_STACK_INCLUSIVE);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+}
